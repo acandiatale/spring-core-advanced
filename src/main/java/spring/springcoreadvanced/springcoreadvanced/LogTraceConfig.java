f451e82792b06d40926475c077b7c1bbea55de7b
jawsubak <acandiatale@gmail.com>
2022-06-26 04:31:14 +0900

    ThreadLocal

--- src/main/java/spring/springcoreadvanced/springcoreadvanced/LogTraceConfig.java
+++ src/main/java/spring/springcoreadvanced/springcoreadvanced/LogTraceConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.springcoreadvanced.springcoreadvanced.trace.logtrace.FieldLogTrace;
 import spring.springcoreadvanced.springcoreadvanced.trace.logtrace.LogTrace;
+import spring.springcoreadvanced.springcoreadvanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
